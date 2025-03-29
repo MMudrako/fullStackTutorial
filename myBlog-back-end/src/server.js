eac19378f4d571c972be5dd0d681ebaed365f10f
MMudrako <124760922+MMudrako@users.noreply.github.com>
2025-03-29 00:19:41 +0000

    nodemon installation

--- myBlog-back-end/src/server.js
+++ myBlog-back-end/src/server.js
@@ -1,10 +1,51 @@
 import express from 'express';
 
+const articleInfo = [
+    { name: 'learn-node', upvotes: 0, comments: []},
+    { name: 'learn-react', upvotes: 0, comments: []},
+    { name: 'mongodb', upvotes: 0, comments: []},
+]
+
 const app = express();
 
-app.get('/hello', function (req, res) {
-    res.send('Hello!')
+app.use(express.json());
+
+/* app.get('/hello', function (req, res) {
+    res.send('Hello from GET endpoint!')
 });
+
+app.get('/hello/:name', function(req, res) {
+    res.send('Hello, ' + req.params.name)
+})
+
+app.post('/hello', function(req, res) {
+    res.send('Hello, '+ req.body.name +' from a POST endpoint!')
+})
+ */
+
+app.post('/api/articles/:name/upvote', (req, res) => {
+    const article = articleInfo.find(a => a.name === req.params.name);
+    article.upvotes += 1;
+
+    //res.send('Hey! The article ' + req.params.name + 'now has ' + article.upvotes + ' upvotes!')
+    res.json(article);
+});
+
+app.post('/api/articles/:name/comments', (req, res) => {
+    const {name } = req.params;
+    const {postedBy, text} = req.body;
+
+    const article = articleInfo.find(a => a.name === name);
+
+    article.comments.push({
+        postedBy,
+        text
+    });
+
+    res.json(article);
+
+})
+
 
 app.listen(8000, function () {
     console.log("server is listening on port 8000")
